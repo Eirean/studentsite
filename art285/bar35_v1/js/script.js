c57e175d26989b79a3d8858101bbbe76a6ad9a8d
Eirean <patyjewi@hawaii.edu>
2024-01-11 20:45:44 -1000

    i dont know

---
+++ art285/bar35_v1/js/script.js
@@ -0,0 +1,30 @@
+console.log("hello");
+
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute("data-sectionstate", "active");
+        } else {
+            entry.target.setAttribute("data-sectionstate", "inactive");
+        }
+    });
+});
+
+
+document.querySelectorAll(".animate-me-on-scroll").forEach((section) => {
+    myobserver.observe(section);
+});
+
+// TOGGLE THE MAIN MENU VIA MENU BUTTON
+const mymenubutton = document.querySelector('.site-header .menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
+
+mymenubutton.onclick = function() {
+    // if nav is open, then close it; else open it
+    if ( mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute( 'data-navstate','closed');
+    } else {
+        mysitenav.setAttribute('data-navstate','open');
+    }
+};
---
+++ art285/bar35_v2/README.md
@@ -0,0 +1,4 @@
+# personal_framework
+A personal set of starter HTML, CSS, amd JS for a modern static site.
+
+[View Demo](https://eirean.github.io/personal_framework)
